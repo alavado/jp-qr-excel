6bc41bd28202095b357546d82906eaf2168b4e3b
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-03 17:21:19 -0300

    agrega beep

--- src/components/LeerQR/LeerQR.js
+++ src/components/LeerQR/LeerQR.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import QrReader from 'react-qr-reader'
 import { Link } from 'react-router-dom'
+import beep from '../../assets/beep.wav'
 import './LeerQR'
 
 const LeerQR = () => {
 
   const [data, setData] = useState('')
+  const audio = new Audio(beep)
 
   const handleScan = data => {
     if (data) {
       setData(data)
+      audio.play()
     }
   }
 
