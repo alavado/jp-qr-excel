833fc9f24e970b38db30aeb8b72d75614a0a70a0
Alejandro Lavado <alejandro.lvd@gmail.com>
2019-12-03 17:25:29 -0300

    agrega media query

--- src/components/LeerQR/LeerQR.js
+++ src/components/LeerQR/LeerQR.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import QrReader from 'react-qr-reader'
 import { Link } from 'react-router-dom'
 import beep from '../../assets/beep.wav'
-import './LeerQR'
+import './LeerQR.css'
 
 const LeerQR = () => {
 
@@ -21,19 +21,19 @@ const LeerQR = () => {
   }
 
   return (
-    <div>
+    <div className="contenedor">
       <QrReader
-          delay={300}
-          onError={handleError}
-          onScan={handleScan}
-          style={{ marginTop: '10vw', width: '80vw' }}
-        />
-        <h1>Aquí abajo aparece lo leído</h1>
-        <p>{data}</p>
-        <div className="formulario">
-          <Link to="/"><button>Agregar registro</button></Link>
-          <Link to="/"><button>Cancelar</button></Link>
-        </div>
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        className="qr-reader"
+      />
+      <h1>Aquí abajo aparece lo leído</h1>
+      <p>{data}</p>
+      <div className="formulario">
+        <Link to="/"><button>Agregar registro</button></Link>
+        <Link to="/"><button>Cancelar</button></Link>
+      </div>
     </div>
   )
 }
